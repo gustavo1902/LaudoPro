7480757a167009ed932c4a0017c2d805ff710115
gustavo1902 <gustavomorais1009@gmail.com>
2025-07-27 15:12:05 -0300

    feat(frontend): configura roteamento principal em app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login'; 
 
-export const routes: Routes = [];
+// Rotas de dashboard e 404
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { PatientDashboardComponent } from './patient/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+
+import { AuthGuard } from './core/auth.guard'; // Caminho para o AuthGuard
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin/dashboard',
+    component: AdminDashboardComponent,
+    canActivate: [AuthGuard],
+    data: { role: 'ROLE_ADMIN' }
+  },
+  {
+    path: 'patient/dashboard',
+    component: PatientDashboardComponent,
+    canActivate: [AuthGuard],
+    data: { role: 'ROLE_PACIENTE' }
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
