6abda87432a46d8cc6c7befdd506e87a7cce9104
gustavo1902 <gustavomorais1009@gmail.com>
2025-08-07 12:49:12 -0300

    feat (frontend): incluindo a habilitação do roteador, o cliente HTTP e a injeção do AuthInterceptor para lidar com o token JWT em todas as requisições.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { routes } from './app.routes'; 
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './core/auth-interceptor';
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, FormsModule, ReactiveFormsModule) 
+    provideHttpClient(withInterceptors([AuthInterceptor])),
+    importProvidersFrom(CommonModule),
   ]
 };
