6467b277073df71b161210844a38b91607f1019f
gustavo1902 <gustavomorais1009@gmail.com>
2025-08-07 13:10:52 -0300

    feat (frontend): adcionando getUserRole

--- frontend/src/app/core/auth-guard.ts
+++ frontend/src/app/core/auth-guard.ts
@@ -1,37 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth'; 
+import { AuthService } from './auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     if (this.authService.isAuthenticated()) {
       const expectedRole = route.data['role'];
+      const userRole = this.authService.getUserRole();
 
-      if (expectedRole) {
-        const userProfile = this.authService.getUserProfile();
-        // A role no `data` da rota é uma string simples ('ADMIN', 'PACIENTE').
-        // A role retornada por `getUserProfile` é 'ROLE_ADMIN' ou 'ROLE_PACIENTE'.
-        // Precisamos que os formatos coincidam para a comparação.
-        // Ajuste aqui para comparar corretamente:
-        if (`ROLE_${(expectedRole as string).toUpperCase()}` === userProfile) {
-          return true;
-        } else {
-          console.warn(`Acesso negado: Perfil ${userProfile} não tem acesso a esta rota. Requer: ${expectedRole}`);
-          this.router.navigate(['/']);
-          return false;
-        }
+      if (expectedRole && userRole && userRole.toUpperCase() === expectedRole.toUpperCase()) {
+        return true;
+      } else {
+        console.warn(`Acesso negado: Perfil ${userRole} não tem acesso a esta rota. Requer: ${expectedRole}`);
+        this.router.navigate(['/']);
+        return false;
       }
-      return true;
     } else {
       console.log('Não autenticado, redirecionando para login.');
       this.router.navigate(['/login']);
