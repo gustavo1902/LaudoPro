c86590a24436af8455a0ac64b841aab149bd1914
gustavo1902 <gustavomorais1009@gmail.com>
2025-08-07 12:56:32 -0300

    feat (frontend): Novas rotas adicionadas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,35 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login'; 
-
-// Rotas de dashboard e 404
+import { LoginComponent } from './auth/login/login';
 import { AdminDashboard } from './admin/admin-dashboard/admin-dashboard';
 import { PatientDashboard } from './patient/dashboard/dashboard';
 import { PageNotFound } from './shared/page-not-found/page-not-found';
-
 import { AuthGuard } from './core/auth-guard';
+import { PatientManagement } from './admin/patient-management/patient-management';
+import { ExamManagement } from './admin/exam-management/exam-management';
+import { MyExams} from './patient/my-exams/my-exams';
+import { PatientProfile } from './patient/patient-profile/patient-profile';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: 'admin/dashboard',
-    component: AdminDashboard,
+    path: 'admin',
     canActivate: [AuthGuard],
-    data: { role: 'ROLE_ADMIN' }
+    data: { role: 'ADMIN' },
+    children: [
+      { path: 'dashboard', component: AdminDashboard },
+      { path: 'pacientes', component: PatientManagement },
+      { path: 'exames', component: ExamManagement },
+    ]
   },
   {
-    path: 'patient/dashboard',
-    component: PatientDashboard,
+    path: 'paciente',
     canActivate: [AuthGuard],
-    data: { role: 'ROLE_PACIENTE' }
+    data: { role: 'PACIENTE' },
+    children: [
+      { path: 'dashboard', component: PatientDashboard },
+      { path: 'meus-exames', component: MyExams },
+      { path: 'perfil', component: PatientProfile },
+    ]
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: PageNotFound }
