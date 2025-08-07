7365981f73296d4499d93a0700ca7fc519c10602
gustavo1902 <gustavomorais1009@gmail.com>
2025-08-07 12:58:34 -0300

    feat (frontend): usar o novo componente de cabeçalho

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend-app');
+  protected readonly title = signal('LaudoPro');
 }
