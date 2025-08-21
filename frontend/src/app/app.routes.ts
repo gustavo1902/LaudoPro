6a6ceca931a5149d6107af318a160baba04699f4
gustavo1902 <gustavomorais1009@gmail.com>
2025-08-20 21:30:00 -0300

    feat (frontend): criando a rota para tela de registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
+import { RegistrationComponent } from './auth/registration/registration.component';
 import { AdminDashboard } from './admin/admin-dashboard/admin-dashboard';
 import { PatientDashboard } from './patient/dashboard/dashboard';
 import { PageNotFound } from './shared/page-not-found/page-not-found';
@@ -11,6 +12,7 @@ import { PatientProfile } from './patient/patient-profile/patient-profile';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistrationComponent },
   {
     path: 'admin',
     canActivate: [AuthGuard],
