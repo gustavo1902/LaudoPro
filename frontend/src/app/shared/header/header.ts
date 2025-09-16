d26a014e0b11db366516f9cb5496c5cc8d499588
gustavo1902 <gustavomorais1009@gmail.com>
2025-09-16 15:26:42 -0300

    refactor: add login e registro

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/auth';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html', 
   styleUrls: ['./header.css']    
 })
